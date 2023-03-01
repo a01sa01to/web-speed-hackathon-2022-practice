da5521d62d2434e8f8a99f6d1aefce73eaffae16
Asa <26807394+a01sa01to@users.noreply.github.com>
2023-03-01 21:39:08 +0900

    Remove axios

--- src/client/foundation/utils/HttpUtils.js
+++ src/client/foundation/utils/HttpUtils.js
@@ -1,8 +1,13 @@
-import axios from "axios";
-
 export const jsonFetcher = async (/** @type {string} */ url) => {
-  const res = await axios.get(url, { responseType: "json" });
-  return res.data;
+  const res = await fetch(url, {
+    headers: { "Content-Type": "application/json" },
+  }).then(res => {
+    if (!res.ok) {
+      throw new Error("failed to fetch");
+    }
+    return res.json();
+  })
+  return res;
 };
 
 /**
@@ -10,9 +15,13 @@ export const jsonFetcher = async (/** @type {string} */ url) => {
  * @param {string} userId
  */
 export const authorizedJsonFetcher = async (url, userId) => {
-  const res = await axios.get(url, {
-    headers: { "x-app-userid": userId },
-    responseType: "json",
-  });
-  return res.data;
+  const res = await fetch(url, {
+    headers: { "Content-Type": "application/json", "x-app-userid": userId },
+  }).then(res => {
+    if (!res.ok) {
+      throw new Error("failed to fetch");
+    }
+    return res.json();
+  })
+  return res;
 };
