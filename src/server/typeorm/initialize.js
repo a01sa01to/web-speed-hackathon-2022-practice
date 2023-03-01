c76148eefa63577137318a47a979e462a81cd33e
Asa <26807394+a01sa01to@users.noreply.github.com>
2023-03-01 22:51:30 +0900

    update: dbにindexを張る

--- src/server/typeorm/initialize.js
+++ src/server/typeorm/initialize.js
@@ -1,7 +1,21 @@
 import fs from "fs/promises";
 
+import sqlite3 from "sqlite3";
+
 import { DATABASE_PATH, INITIAL_DATABASE_PATH } from "../paths.js";
 
 export async function initialize() {
   await fs.copyFile(INITIAL_DATABASE_PATH, DATABASE_PATH);
+
+  const db = new sqlite3.Database(DATABASE_PATH);
+  const run = (query) => new Promise((resolve, reject) => {
+    db.run(query, (error) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve();
+      }
+    });
+  });
+  await run("CREATE INDEX idx_race_id ON odds_item (raceId);");
 }
